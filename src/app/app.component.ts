9c5743e712d7fdf4f66a209b19a72eeaa4281324
Sellsword9 <yerayromeroortiz@gmail.com>
2023-11-20 22:40:38 +0100

    Angular Init

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { invoke } from "@tauri-apps/api/tauri";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  greetingMessage = "";
+
+}
